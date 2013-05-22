084fa9730a7de4684de2bca0d9606c1f3872aa94
scottjson <scottjson@server.fake>
2013-05-22 18:09:10 +1000

    updated license and lib

---
+++ README.md
@@ -0,0 +1,33 @@
+mthA
+====
+
+# What #
+
+to assist in simple maths
+
+# Why? #
+
+For a lot of people symbols and calculations are confusing.
+
+Why not replace symbols etc with words that can be understood and quicker to type then working out the calculation.
+
+mthA.js makes the process easier.
+
+
+# Usage #
+
+To use:
+
+	var result = mthA(20,'%off',50);
+	var result2 = mthA(60,'toradians');
+
+# Goals #
+
+## Easy to use ##
+
+## Tiny ##
+
+## Fast ##
+
+# License #
+See LICENSE
---
+++ mthA.js
@@ -0,0 +1,44 @@
+//Version: 1.0,
+//Author: SCOTTJSON
+//Created: MAY 2013
+
+function mthA(number1, task, number2) {
+    
+    var result = undefined;
+    
+    switch(task){
+        case "times":
+            result = number1*number2;
+            break;
+
+        case "divide":
+            result = number1/number2;
+            break;
+
+        case "remainder":
+            result = number1%number2;
+            break; 
+
+        case "percentof":
+            result = (number1/number2)*100;
+            break; 
+
+        case "%off":  //20% off $50 = Answer: $40
+            result = number2*(1-(number1/100));
+            break; 
+
+        case "toradians":
+            var pi = Math.PI;
+            result = number1*(pi/180);
+            break;
+    }
+    return result;
+};
+
+function mthACalculate(){
+    var number1 = document.getElementById("mthAnumber1").value;
+    var number2 = document.getElementById("mthAnumber2").value;
+    var task = document.getElementById("mthATask").value;
+    var result = document.getElementById("mthAresult");
+    result.value = mthA(number1, task, number2);
+}
